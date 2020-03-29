6288b7532f5fb99c7e4160d790dbf7bc054bd574
= <maxharitonov@gmail.com>
2020-03-29 23:22:05 +0300

    reader implementation example

---
+++ src/readerSample.go
@@ -0,0 +1,20 @@
+package main
+/**
+пример реализации интерфейса Reader
+бесконечный поток символов 'A'
+*/
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (r MyReader) Read(b []byte) (int, error) {
+	for i,_ := range b {
+		b[i] = 'A'
+	}
+	return len(b),nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
