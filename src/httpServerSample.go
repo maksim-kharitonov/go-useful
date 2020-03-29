40c7d9c4f7a53e7dfe5f79429dc46ef19932a672
= <maxharitonov@gmail.com>
2020-03-29 22:14:04 +0300

    1. iterate 2dim array 2. fib with closure 3. sample http server with closure

---
+++ src/httpServerSample.go
@@ -0,0 +1,28 @@
+package main
+/**
+Используем замыкание для логирования времени выполнения запроса
+*/
+import (
+  "fmt"
+  "net/http"
+  "time"
+)
+
+func main() {
+  http.HandleFunc("/hello", timed(hello))
+  http.ListenAndServe(":3000", nil)
+}
+
+func timed(f func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    start := time.Now()
+    f(w, r)
+    end := time.Now()
+    fmt.Println("The request took", end.Sub(start))
+  }
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "<h1>Hello!</h1>")
+  fmt.Fprintln(w, "<p>Тестовый ответ от сервера на кириллице</p>")
+}
